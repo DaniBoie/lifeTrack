030f95244d7706e33a6037f67ee9de830b6b8126
Daniel A <dannyboy3385@gmail.com>
2020-08-31 11:05:12 -0700

    diary routes finished

--- routes/diaryRoutes.js
+++ routes/diaryRoutes.js
@@ -1,10 +1,27 @@
 const router = require('express').Router()
 const { Diary } = require('../models')
 
+// GET all diary entries.
 router.get('/diary', (req, res) => {
   Diary.findAll()
     .then(diary => res.json(diary))
     .catch(err => console.log(err))
 })
+
+// CREATE a diary entry
+  router.post('/diary', (req, res) => {
+    Diary.create(req.body)
+    .then(diary => {
+      res.json(diary)
+    })
+    .catch(err => console.log(err))
+  })
+
+// DELETE a diary entry
+  router.delete('/diary/:id', (req,res) => {
+    Diary.destroy({where: {id: req.params.id}})
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
+  })
 
 module.exports = router
