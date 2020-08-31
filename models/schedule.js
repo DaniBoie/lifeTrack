620771b78d7999d670f952b8c5760da0432e3275
Daniel A <dannyboy3385@gmail.com>
2020-08-31 11:21:58 -0700

    schedule routes finished

--- models/schedule.js
+++ models/schedule.js
@@ -5,7 +5,7 @@ class Schedule extends Model { }
 
 Schedule.init({
   entry: { type: DataTypes.STRING, allowNull: false },
-  dateFor : {type: DataTypes.DATE, allowNull: false },
+  dateFor : {type: DataTypes.DATEONLY, allowNull: false },
   urgency: { type: DataTypes.INTEGER, allowNull: false }
 }, { sequelize, modelName: 'schedule' })
 
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,9 +1,32 @@
 const { Schedule } = require('../models')
 const router = require('express').Router()
 
+// GET all schedule entries
 router.get('/schedule', (req, res) => {
   Schedule.findAll()
     .then(schedule => res.json(schedule))
+    .catch(err => console.log(err))
+})
+
+// CREATE a schedule entry
+router.post('/schedule', (req,res) => {
+  Schedule.create(req.body)
+  .then(entry => res.json(entry))
+  .catch(err => console.log(err))
+})
+
+
+// UPDATE a schedule entry
+router.put('/schedule/:id', (req,res) => {
+  Schedule.update(req.body, {where: {id: req.params.id}})
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
+})
+
+// DELETE a schedule entry
+router.delete('/schedule/:id', (req,res) => {
+  Schedule.destroy({where : {id: req.params.id}})
+    .then(() => res.sendStatus(200))
     .catch(err => console.log(err))
 })
 
