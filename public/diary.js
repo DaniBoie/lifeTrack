c90152487d08edc695317ee06acd15faefba656e
Daniel A <dannyboy3385@gmail.com>
2020-09-02 15:32:41 -0700

    Diary page js finished

--- public/diary.js
+++ public/diary.js
@@ -2,15 +2,41 @@
 // moment().format(); 
 
 axios.get('./api/diary')
-  .then(({data}) => {
-    var currentDay = moment("2020-11-02").format("L");
-    
+  .then(({ data }) => {
+    var currentDay = moment().format("L");
+
     let indexVal = data.findIndex(data => moment(data.createdAt.substr(0, 10)).format("L") === currentDay);
 
     console.log(indexVal);
     var entryDate = moment(data[indexVal].createdAt.substr(0, 10)).format("L");
 
-    document.getElementById('diaryDate').innerText = entryDate;
+    document.getElementById('diaryDate').innerText = `Today's Entry: ${entryDate}`;
     document.getElementById('diaryRender').innerText = data[indexVal].entry;
   })
-  .catch(err => console.log(err))
+  .catch(err => {
+    console.log(err)
+    document.getElementById('diaryDate').innerText = `Today's Entry: ${moment().format("L")}`;
+    document.getElementById('diaryRender').innerText = 'No Entry Added Today!';
+  })
+
+document.getElementById('dateSubmit').addEventListener('click', (event) => {
+  event.preventDefault()
+
+  axios.get('./api/diary')
+    .then(({ data }) => {
+      var findDay = moment(document.getElementById('date').value).format("L");
+
+      let indexVal = data.findIndex(data => moment(data.createdAt.substr(0, 10)).format("L") === findDay);
+
+      console.log(indexVal);
+      var entryDate = moment(data[indexVal].createdAt.substr(0, 10)).format("L");
+
+      document.getElementById('diaryDate').innerText = findDay;
+      document.getElementById('diaryRender').innerText = data[indexVal].entry;
+    })
+    .catch(err => {
+      console.log(err)
+      document.getElementById('diaryRender').innerText = "No entry found for this date!"
+      document.getElementById('diaryDate').innerText = moment(document.getElementById('date').value).format("L");
+    })
+})
