2c3b97ccd3f74783a3e913cbc8342e4751643dc7
Daniel A <dannyboy3385@gmail.com>
2020-09-01 19:28:55 -0700

    fixed workout routes

--- models/Workout.js
+++ models/Workout.js
@@ -6,7 +6,9 @@ class Workout extends Model { }
 Workout.init({
   name: { type: DataTypes.TEXT, allowNull: false },
   sets: { type: DataTypes.INTEGER, allowNull: false }, 
-  reps: { type: DataTypes.INTEGER, allowNull: false }
+  reps: { type: DataTypes.INTEGER, allowNull: false },
+  lbs: { type: DataTypes.INTEGER, allowNull: true },
+
 }, { sequelize, modelName: 'workout' })
 
 module.exports = Workout
