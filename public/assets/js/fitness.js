88ddf5cdafaa60aee40e3a83bab86116484a970c
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-03 10:32:54 -0700

    linked fitness file

--- public/assets/js/fitness.js
+++ public/assets/js/fitness.js
@@ -1,32 +1,41 @@
+let workouts = []
+
 document.getElementById('workout').addEventListener('click', (event) => {
-    console.log(document.getElementById('workoutName').value)
-    console.log(document.getElementById('workoutSets').value)
-    console.log(document.getElementById('workoutReps').value)
-    console.log(document.getElementById('workoutLbs').value)
-  
-    let pounds = ''
-    if (document.getElementById('workoutLbs').value === ''){
-      pounds = null
-    } else {
-      pounds = document.getElementById('workoutLbs').value
-    }
-  
-    axios.post('/api/workout', {
-      name: document.getElementById('workoutName').value,
-      sets: document.getElementById('workoutSets').value,
-      reps: document.getElementById('workoutReps').value,
-      lbs: pounds
+
+  let pounds
+  if (document.getElementById('workoutLbs').value === '') {
+    pounds = null
+  } else {
+    pounds = document.getElementById('workoutLbs').value
+  }
+
+  let workout = {
+    name: document.getElementById('workoutName').value,
+    sets: document.getElementById('workoutSets').value,
+    reps: document.getElementById('workoutReps').value,
+    lbs: pounds
+  }
+
+  axios.post('/api/workout', workout)
+    .then(function ({ data }) {
+      console.log(data)
+      workouts.push(data.id)
+      console.log(workouts)
+      let workoutElem = document.createElement('li')
+      workoutElem.innerHTML = `
+      <p data-id="${data.id}">${workout.name} | Sets: ${workout.sets} | Reps: ${workout.reps}</p>
+      `
+      document.getElementById('workouts').append(workoutElem)
     })
-      .then(function (response) {
-        let workoutElem = document.createElement('li')
-        workoutElem.innerHTML = `
-        <p>${document.getElementById('workoutName').value} | Sets: ${document.getElementById('workoutSets').value} | Reps: ${document.getElementById('workoutReps').value}</p>
-        
-        `
-        document.getElementById('workouts').append(workoutElem)
-        console.log(response)
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  })
+    .catch(function (error) {
+      console.log(error);
+    });
+  clearEntries()
+})
+
+function clearEntries() {
+  document.getElementById('workoutName').value = ''
+  document.getElementById('workoutSets').value = ''
+  document.getElementById('workoutReps').value = ''
+  document.getElementById('workoutLbs').value = ''
+}
